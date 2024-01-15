a1c6e99af8a0606287325997ab71d1f484fcb953
thanhdaikho <thanhnppd09326@fpt.edu.vn>
2024-01-15 15:24:15 +0700

    lab1

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import Lab3 from './src/screens/lab3'
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Lab3/>
+      </View>
+    )
+  }
+}
---
+++ src/screens/lab3/index.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+const Lab3 = () => {
+    return(
+        <View>
+            <Text>This is Lab 3</Text>
+        </View>
+    )
+}
+
+export default Lab3 
